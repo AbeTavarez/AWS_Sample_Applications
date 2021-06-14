085961aab46e8bfed0450c6bf2859ac8c8386d1c
AbeTavarez <abeincwireless@gmail.com>
2021-06-13 23:59:52 -0400

    connection added

--- aws_sample_apps-master/WebBrowser/Browser.py
+++ aws_sample_apps-master/WebBrowser/Browser.py
@@ -1,9 +1,19 @@
 import socket
 
 # Create socket
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-socket.connect(('data.pr4e.org', 80))
+browser_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+browser_socket.connect(('data.pr4e.org', 80))
 
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+
+browser_socket.send(cmd)
+
+while True:
+    data = browser_socket.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end='')
+
+browser_socket.close()
 
