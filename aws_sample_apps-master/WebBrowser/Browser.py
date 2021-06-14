0e45779bfabc27ef1c1fdc08fdac47997a01cf58
AbeTavarez <abeincwireless@gmail.com>
2021-06-14 00:01:35 -0400

    getting response

--- aws_sample_apps-master/WebBrowser/Browser.py
+++ aws_sample_apps-master/WebBrowser/Browser.py
@@ -5,7 +5,7 @@ browser_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #
 browser_socket.connect(('data.pr4e.org', 80))
 
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+cmd = 'GET http://google.com HTTP/1.0\r\n\r\n'.encode()
 
 browser_socket.send(cmd)
 
