d1ce15e154c4b579ebc27e75510f424a5977e8f4
AbeTavarez <abeincwireless@gmail.com>
2021-06-14 20:46:32 -0400

    rds settup

--- aws_sample_apps-master/AWS/RDS/rds_db.py
+++ aws_sample_apps-master/AWS/RDS/rds_db.py
@@ -2,25 +2,52 @@ import boto3
 import json
 
 # Vars
-sg_name = 'rds-sg-dev-demo-bk'
-sg_description = 'RDS SG for demo'
-ip_cidr = '0.0.0.0/0'
+sg_name = 'sg-03507e7c60d2e8f76'
+rds_id = ''
+db_name = ''
+user_name = ''
+user_passwd = ''
+admin_email = ''
+sg_id_num = ''
+rds_endpoint = ''
 
-# Create ec2 instance
+
+# 
 ec2_client = boto3.client('ec2')
 
-res = ec2_client.create_security_group(
-    Description=sg_description,
-    GroupName=sg_name)
-
-print(json.dumps(res, indent=2, sort_keys=True))
-
-# SG rule
-res = ec2_client.authorize_security_group_ingress(
-    CidrIp=ip_cidr,
-    FromPort=3306,
-    GroupName=sg_name,
-    ToPort=3306,
-    IpProtocol='tcp'
+res = ec2_client.describe_security_groups(
+    GroupsNames = [sg_name]
 )
-print('SG has been created check console...')
+
+sg_id_num = json.dumps(res['SecurityGroups'][0]['GroupId'])
+sg_id_num = sg_id_num.replace('"','')
+
+# RDS Client
+rds_client = boto3.client('rds')
+# RDS Setup
+res = rds_client.create_dn_instances(
+    DBInstanceIdentifier=rds_id,
+    DBName=db_name,
+    Engine='mariadb',
+    MasterUserName='masteruser',
+    MasterUserPassword='mymasterpassw0rd1!',
+    VpcSecurityGroupsIds=[
+        sg_id_num
+    ],
+    AllocatedStorage=20,
+    Tags=[{
+        'Key': 'POC-Email',
+        'Value': admin_email
+    },
+    {
+        'Key': 'Purpose',
+        'value': 'RDS demo app'
+    }
+    ]
+)
+
+#   Create DB
+print('Creating the RDS Instace, this can take a few minutes...')
+waiter = rds_client.get_waiter('db_instance_available')
+waiter.wait(DBInstanceIdentifier=rds_id)
+print('Ok, you Amazon RDS is ready now!')
